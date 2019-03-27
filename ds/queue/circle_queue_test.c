4a7260a547848b4634b5aec8e8e5c1599cb06a76
Vu Tang <tangvubk@gmail.com>
2019-03-28 00:06:01 +0700

    Makefile and unit test

--- ds/queue/circle_queue_test.c
+++ ds/queue/circle_queue_test.c
@@ -1,14 +1,18 @@
 /*
 * @Author: vutang
 * @Date:   2019-03-27 18:37:59
-* @Last Modified by:   vutang
-* @Last Modified time: 2019-03-27 18:40:27
+* @Last Modified by:   Vu Tang
+* @Last Modified time: 2019-03-28 00:03:15
 */
 
 #include <stdio.h>
 #include <string.h>
+#include <errno.h> 
 
 #include "circle_queue.h"
+
+#define LOGFILE_HEADER "./logcmd/log"
+#define INPUT_DATA "./data/Tuan1_input.txt"
 
 int main() {    
 	queue_t cmd_queue;
@@ -19,12 +23,9 @@ int main() {
 
 	FILE *fin, *fout;
 
-	if ((fin = fopen("Tuan1_input.txt", "r")) < 0) {
-		fprintf(stderr, "Cannot open file: %s\n", "Tuan1_input.txt");
-		return 0;
-	}
-	if ((fout = fopen("./logcmd/log0", "w")) < 0) {
-		fprintf(stderr, "Cannot open file: %s\n", "./logcmd/log0");   
+	if ((fin = fopen(INPUT_DATA, "r")) == NULL) {
+		fprintf(stderr, "Cannot open file: %s, errno: %d (%s)\n", INPUT_DATA, \
+			errno, strerror(errno));
 		return 0;
 	}
 
@@ -33,36 +34,40 @@ int main() {
 
 	while (fgets(str, STRING_LENGTH, fin) != NULL) {
 		if (en_queue(&cmd_queue, str) < 0) {
-			if (i > 0) {
-				fclose(fout);
-				logid[0] = (char) i + '0';                 
-				filename = strcat(strcpy(header, LOGFILE_HEADER), logid);
-				if ((fout = fopen(filename, "w")) < 0) {
-					fprintf(stderr, "Cannot open file: %s\n", filename);
-					return 0;   
-				}
+			logid[0] = (char) (i + 'A');
+			filename = strcat(strcpy(header, LOGFILE_HEADER), logid);
+			if ((fout = fopen(filename, "w")) == NULL) {
+				fprintf(stderr, "Cannot open file: %s\n", filename);
+				return 0;   
 			}
 			i++;
 
-			while (de_queue(&cmd_queue, str_tmp) > 0) {
+			while (de_queue(&cmd_queue, str_tmp) >= 0) {
 				fprintf(fout, "%s", str_tmp);
 			}
+			fclose(fout);
 			en_queue(&cmd_queue, str);
 		}
 	}
 
-	/*Flush queue at the end of working session*/
-	fclose(fout);
-	logid[0] = (char) i + '0';                 
-	filename = strcat(strcpy(header, LOGFILE_HEADER), logid);
-	if ((fout = fopen(filename, "w")) < 0) {
-	fprintf(stderr, "Cannot open file: %s\n", filename);   
-	return 0;
-	}
-	while (de_queue(&cmd_queue, str_tmp) > 0) {
-	fprintf(fout, "%s", str_tmp);
-	}
+	fclose(fin);
 
-	fclose(fin); fclose(fout);
+	/*Flush queue at the end of working session*/
+	if (!is_empty(&cmd_queue)) {
+		logid[0] = (char) (i + 'A');
+
+		filename = strcat(strcpy(header, LOGFILE_HEADER), logid);
+		printf("%s\n", filename);
+
+		if ((fout = fopen(filename, "w")) < 0) {
+			fprintf(stderr, "Cannot open file: %s\n", filename);   
+			return 0;
+		}
+		while (de_queue(&cmd_queue, str_tmp) >= 0) 
+			fprintf(fout, "%s", str_tmp);
+
+		fclose(fout);
+	}
+	
 	return 1;
 }
