e94df8a8ab2c6a186087ac59d5716e0dd0762a57
vutang <tangvubk@gmail.com>
2019-04-01 19:04:21 +0700

    stack_list: coding...

---
+++ ds/stack/stack_list.c
@@ -0,0 +1,7 @@
+/*
+* @Author: vutang
+* @Date:   2019-04-01 18:41:19
+* @Last Modified by:   vutang
+* @Last Modified time: 2019-04-01 18:43:11
+*/
+#include "../linked_list/ll_singly.h"
---
+++ ds/stack/stack_list.h
@@ -0,0 +1,5 @@
+int stack_init(int size);
+void stack_del(void);
+int stack_is_empty(void);
+int stack_push(int);
+int stack_pop(void);
