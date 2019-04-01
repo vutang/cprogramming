e9283f14d6b608e00fe5ce3ac96b7985274e25a1
Vu Tang <tangvubk@gmail.com>
2019-04-01 23:26:58 +0700

    stack list

--- ds/stack/stack_list.h
+++ ds/stack/stack_list.h
@@ -1,5 +1,5 @@
-int stack_init(int size);
-void stack_del(void);
-int stack_is_empty(void);
-int stack_push(int);
-int stack_pop(void);
+int stack_list_init();
+void stack_list_del(void);
+int stack_list_is_empty(void);
+int stack_list_push(int);
+int stack_list_pop(int *dout);
