d11709bcecc5653bd870e1f919f5d81234534435
vutang <tangvubk@gmail.com>
2019-04-01 18:40:07 +0700

    re-order stack

--- ds/stack/stack_array.c
+++ ds/stack/stack_array.c
@@ -1,8 +1,8 @@
 /*
 * @Author: Vu Tang
 * @Date:   2019-03-31 22:37:02
-* @Last Modified by:   Vu Tang
-* @Last Modified time: 2019-03-31 23:46:54
+* @Last Modified by:   vutang
+* @Last Modified time: 2019-04-01 18:37:00
 */
 #define DEBUG
 
@@ -11,10 +11,10 @@
 #include <errno.h>
 #include <string.h>
 
-#include "stack.h"
+#include "stack_array.h"
 
 #ifdef DEBUG
-#define pr_dbg(s, ...) 	printf("[DEBUG] "); \
+#define pr_dbg(s, ...) 	printf("%s:#%d [DBG] ", __FILE__, __LINE__); \
 						printf(s, __VA_ARGS__);
 #else
 #define pr_dbg(s, ...)
@@ -73,7 +73,7 @@ int stack_push(int d_in)
 	if (stk_ptr == NULL)
 		return -1;
 	stk_ptr[stk_cur_pos++] = d_in;
-	pr_dbg("add %d d_in to %d\n", d_in, stk_cur_pos - 1);
+	pr_dbg("push d_in %d to %d\n", d_in, stk_cur_pos - 1);
 	return 0;
 }
 
