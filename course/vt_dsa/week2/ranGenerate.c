ac7fbbf87aa71bcf73dc05d1c24dd2e5ccba00b1
Vu Tang <tangvubk@gmail.com>
2019-04-25 00:31:40 +0700

    upgrade random generator

--- course/vt_dsa/week2/ranGenerate.c
+++ course/vt_dsa/week2/ranGenerate.c
@@ -1,26 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h> /*For getopt*/
 
-#define SIZE 80 /*In MegaByte*/
+#include <sys/types.h>
+#include <sys/stat.h> /*stat()*/
 
-int main(int argc, char *argv[]) {
-	int i, n;
+#include <time.h>
+
+#define DEFAULT_SIZE 80 /*In MegaByte*/
+#define OUTPUT "./uInput.dat"
+
+int main(int argc, char *argv[]) 
+{
+	int i, n, size = -1, opt;
 	char *ptr;
 	FILE *fp;
 
-	if ((fp = fopen("./uInput.dat", "w+")) == NULL) {
+	/*Check file*/
+	struct stat sb;
+
+	/*Time*/
+	clock_t tick1, tick2;
+
+	while ((opt = getopt(argc, argv, "n:")) != -1) {
+		switch (opt) {
+		case 'n':
+			size = atoi(optarg);
+			break;
+		default:
+			break;
+		}
+	}
+
+	if ((fp = fopen(OUTPUT, "w+")) == NULL) {
 		printf("Cannot open file!\n");
 		return 0;
 	}
 
 	srand(time(NULL));
 
-	for (i = 0; i < SIZE * 1024 * 1024 / 4; i++) {
+	if (size <= 0) {
+		printf("Default size: %d\n", DEFAULT_SIZE);
+		size = DEFAULT_SIZE;
+	}
+	else
+		printf("Got size: %d\n", size);
+	
+	tick1 = clock();
+	if (tick1 == -1) {
+		printf("clock() fail\n");
+		exit(1);
+	}
+
+	for (i = 0; i < size * 1024 * 1024 / 4; i++) {
 		n = rand();
 		fwrite(&n, sizeof(int), 1, fp);
 	}
-
 	fclose(fp);
+
+	tick2 = clock();
+	if (tick2 == -1) {
+		printf("clock() fail\n");
+		exit(1);
+	}
+
+	printf("Run time: %.2f (secs)\n",
+		(double) (tick2 - tick1) / CLOCKS_PER_SEC);
+
+	/*Check file info*/
+	if (stat(OUTPUT, &sb) == -1) {
+		printf("stat() fail\n");
+	}
+
+	printf("Actual file size: %d (KBytes)\n", (int) sb.st_size / 1024);
+
 	return 0;
 }
