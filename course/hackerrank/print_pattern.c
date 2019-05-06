2ce09723b6201c305227afbfc476d29bf0b259ff
vutang <tangvubk@gmail.com>
2019-05-06 18:11:03 +0700

    add more hackerrank in c

--- course/hackerrank/print_pattern.c
+++ course/hackerrank/print_pattern.c
@@ -1,8 +1,8 @@
 /*
 * @Author: Vu Tang
 * @Date:   2019-05-05 23:53:32
-* @Last Modified by:   Vu Tang
-* @Last Modified time: 2019-05-06 00:24:09
+* @Last Modified by:   vutang
+* @Last Modified time: 2019-05-06 11:22:54
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,30 +16,25 @@ int main(int argc, char **argv)
 	int i, j, ch;
 
 	for (i = 0; i < n; i++) {
-		for (j = 0; j < n; j++) {
-			if (j < i)
-				ch = n - j;
+		for (j = 0; j < 2 * n - 1; j++) {
+			if ((j >= i) && (j < 2 * n - 1 - i))
+				printf("%d ", n - i);
+			else if (j < i)
+				printf("%d ", n - j);
 			else
-				ch = n - i;
-			printf("%d ", ch);
-		}
-
-		for (j = 0; j < n - 1; j++) {
-			if (j < n - i - 1)
-				ch = n - i;
-			else 
-				ch = j + 2;
-			printf("%d ", ch);	
+				printf("%d ", j - (n - 2));
 		}
 		printf("\n");
 	}
 
-	for (i = n - 1; i > 0; i--) {
-		for (j = 0; j < n; j++) {
-			ch = n - j;
-			if (j < i)
-				ch = i;
-			printf("%d ", ch);
+	for (i = n - 2; i >= 0; i--) {
+		for (j = 0; j < 2 * n - 1; j++) {
+			if ((j >= i) && (j < 2 * n - 1 - i))
+				printf("%d ", n - i);
+			else if (j < i)
+				printf("%d ", n - j);
+			else
+				printf("%d ", j - (n - 2));
 		}
 		printf("\n");
 	}
