2c09b9838df11b47da501ba10967dc041d179c62
Vu Tang <tangvubk@gmail.com>
2019-08-06 15:03:02 +0700

    [thd_comm_poll] add testing file transfer

--- prjs/multi_thd_comm/thd_comm_poll.c
+++ prjs/multi_thd_comm/thd_comm_poll.c
@@ -3,16 +3,14 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <poll.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include "circle_queue_array.h"
+#include "../logger/logger.h"
 
 #define TIMEOUT 5
-
-#ifdef DEBUG
-#define pr_debug(...) printf("%s", __VA_ARGS__)
-#else
-#define pr_debug(...)
-#endif
 
 /*Pipes for poll*/
 static int pfd[2][2];
@@ -40,68 +38,92 @@ static void *siggen_thd_func()
 static void *pcie_thd_func() 
 {
 	struct pollfd plfd;
-	int ret, cnt = 0;
+	int ret;
 	char c;
-	char str[STRING_LENGTH];
+	// char buf[STRING_LENGTH];
+	struct queue_element q_elm;
 
 	plfd.fd = pfd[1][0];
 	plfd.events = POLLIN;	
 
+	int fd = open("./tmp/output.jpg", O_CREAT | O_RDWR);
+	if (fd < 0)
+		perror("open(pcie_thd_id):");
 
 	while (1) {
 		ret = poll(&plfd, 1, TIMEOUT * 1000);
 		if (ret == -1) {
 			perror ("poll");
-			return NULL;
+			break;
 		}
 
 		if (!ret) {
 			printf ("%d seconds elapsed.\n", TIMEOUT);
-			return NULL;
+			break;
 		}
 
 		if (plfd.revents & POLLIN) {
-			pr_debug("PCIE got signal\n");
-			sprintf(str, "Test string %d", cnt++);
-			ret = en_queue(&dl_queue, str);
-			if (ret)
-				pr_debug("Queue full\n");
-
+			LOG_DEBUG("PCIE got signal\n");
+			ret = de_queue(&dl_queue, &q_elm);
+			if (ret) {
+				LOG_DEBUG("de_queue fail\n");
+				break;
+			}
+			/*Write back to file*/
+			write(fd, q_elm.block, q_elm.size);
 			/*Clear Pipe*/
 			read(pfd[1][0], &c, 1);
 		}
 	}
+	close(fd);
+	return NULL;
 }
 
 static void *l1_thd_func()
 {
 	struct pollfd plfd;
 	int ret;
+	// int cnt = 0;
 	char c;
-	char str[STRING_LENGTH];
+	// char buf[STRING_LENGTH];
 
+	/*Init for polling*/
 	plfd.fd = pfd[0][0];
 	plfd.events = POLLIN;	
 
+	struct queue_element q_elm;
+
+	int fd = open("./tmp/input.jpg", O_RDONLY);
+	if (fd == -1) {
+		perror("open:");
+		return NULL;
+	}
 
 	while (1) {
 		ret = poll(&plfd, 1, TIMEOUT * 1000);
 		if (ret == -1) {
 			perror ("poll");
-			return NULL;
+			break;
 		}
 
 		if (!ret) {
 			printf ("%d seconds elapsed.\n", TIMEOUT);
-			return NULL;
+			break;
 		}
 
 		if (plfd.revents & POLLIN) {
-			pr_debug("L1 got signal\n");
-			ret = de_queue(&dl_queue, str);
+			LOG_DEBUG("L1 got signal\n");
+			// sprintf(buf, "Test string %d", cnt++);
+			ret = read(fd, q_elm.block, STRING_LENGTH);
+			if (ret <= 0) {
+				LOG_DEBUG("End of file");
+				break;
+			}
+			q_elm.size = ret;
+
+			ret = en_queue(&dl_queue, &q_elm);
 			if (ret)
-				printf("de_queue fail\n");
-			printf("PCIe Got: %s\n", str);
+				LOG_WARN("en_queue fail\n");
 			read(pfd[0][0], &c, 1);
 		}
 	}
@@ -112,6 +134,10 @@ int main()
 {
 	pthread_t siggen_thd_id, pcie_thd_id, l1_thd_id;
 	int ret;
+
+	ret = config_log("./thd_comm_poll.log", DEBUG, LOG_TO_SCREEN | LOG_TO_FILE);
+	if (ret < 0)
+		printf("Configure log fail\n");
 
 	/*Create Pipe*/
 	if (pipe(pfd[0]) == -1) {
